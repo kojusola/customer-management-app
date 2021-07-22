b14ca84e4b714c30efafe063f45bddd90449346c
kojusola <adeola5678@gmail.com>
2021-07-22 09:59:04 +0100

    inventory&sales

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,9 @@ import { Route, Switch } from "react-router";
 import Register from "./pages/auth/signup/register"
 import Signin from "./pages/auth/signin/signin";
 import Resetpassword from "./pages/auth/resetpassword/resetpassword";
-import quotes from "./pages/quotes/quotes";
+import Quotes from "./pages/quotes/quotes";
+import Inventory from "./pages/inventory/inventory";
+import Sales from "./pages/sales/sales";
 
 function Routes() {
     return (
@@ -11,7 +13,9 @@ function Routes() {
        <Route path="/" exact component={Register} />
        <Route path="/signin" exact component={Signin} />
        <Route path="/password-reset" exact component={Resetpassword } />
-       <Route path="/quotes" exact component={quotes} />
+       <Route path="/quotes" exact component={Quotes} />
+       <Route path="/inventory" exact component={Inventory} />
+       <Route path="/sales" exact component={Sales} />
       </Switch>
     );
 }
